66a760bee8028f7df3424f7b8f2d299d5ec25fb7
Sebastian Zarate <zaratesanmiguel2019@outlook.com>
2023-09-23 23:22:22 -0500

    Added Bootstrap Structure
    Both views have their structure ready to implement functionality.

---
+++ src/ts/index.ts
@@ -0,0 +1,5 @@
+import * as $ from "jquery";
+
+$("#main_button").on("click",()=>{
+    window.location.href = "./pages/general.html";
+});
---
+++ src/utils/pageCommonInit.ts
@@ -0,0 +1,14 @@
+/*
+    Esta función permite inicializar elementos comunes en una página del módulo de juego en línea.
+    Ejemplos son:
+
+    1. Cargar la NavBar
+*/
+
+import $ from "jquery";
+
+//1.Cargar la NavBar
+window.onload = function(){
+    $("#navbar").load("../layouts/navbar.html");
+}
+
