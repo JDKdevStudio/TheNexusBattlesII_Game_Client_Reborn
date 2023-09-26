8bc91bd50ed3340dfc6c4e9c9eae20b9bd3d090f
Sebastian Zarate <zaratesanmiguel2019@outlook.com>
2023-09-26 00:40:49 -0500

    Added Party Search

--- src/ts/general.ts
+++ src/ts/general.ts
@@ -1,9 +1,43 @@
 import * as $ from "jquery";
 import 'bootstrap/dist/js/bootstrap.bundle.js';
+import { ColyseusNexusClient } from "../utils/colyseusClient";
+import { NexusRoom } from "../types/nexusRoom";
 
-$("#btn-create-room").on("click",function () {
-    $("#modal-create-room").load("../components/roomConfigComponent/roomConfig.html",()=>{
+$("#btn-create-room").on("click", function () {
+    $("#modal-create-room").load("../components/roomConfigComponent/roomConfig.html", () => {
         $("#roomConfig").show();
     });
 })
 
+const loadRoomDataOnList = async () => {
+    $("#party-list").empty();
+    let availableRooms = await ColyseusNexusClient.nexusClientGetAvaliableRooms();
+    if (availableRooms.length > 0) {
+        $.get("../templates/partyListItem.html", function (data: string) {
+            availableRooms.forEach((current_room: NexusRoom) => {
+                let response = data;
+ 
+                let placeholders: Record<string, string> = {
+                    "{{id_placeholder}}": current_room.roomId,
+                    "{{name_placeholder}}": current_room.roomMedatadataNombre,
+                    "{{current_placeholder}}": current_room.roomMetadataClients,
+                    "{{max_placeholder}}": current_room.roomMetadataMaxClients,
+                    "{{mode_placeholder}}": "TBD",
+                    "{{bonus_placeholder}}": current_room.roomMetadataGanancia,
+                };
+
+                for (let placeholder in placeholders) {
+                    if (placeholders.hasOwnProperty(placeholder)) {
+                        console.log(placeholders[placeholder])
+                        response = response.replace(placeholder, placeholders[placeholder]);
+                        console.log(response);
+
+                    }
+                }
+                $('#party-list').append(response);
+            });
+        });
+    }
+}
+
+$('#refresh-button').on('click', loadRoomDataOnList);
---
+++ src/types/nexusRoom.ts
@@ -0,0 +1,7 @@
+export type NexusRoom = {
+    roomId:string,
+    roomMedatadataNombre:string,
+    roomMetadataClients:string,
+    roomMetadataMaxClients:string,
+    roomMetadataGanancia:string
+}
