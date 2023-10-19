7d2769ea90020ebc86eb825e22063cdbec55f931
JDKdev <jose.cabrera.2021@upb.edu.co>
2023-10-19 13:32:46 -0500

    getuserData apiconn finished

--- src/classes/apiConn/apiConn.ts
+++ src/classes/apiConn/apiConn.ts
@@ -4,7 +4,33 @@ import { UserDataType } from "./types/userDataType";
 export default class ApiConn{
         private static AccessToken?:string = Cookies.get("access_token")
         static getUserData=async():Promise<UserDataType>=>{
-            
+            let headersList = {
+                "Content-Type": "application/json",
+                "Authorization": this.AccessToken ?? ""
+               }
+               
+               let response = await fetch("https://webserver.thenexusbattles2.cloud/ver-perfil", { 
+                 method: "GET",
+                 headers: headersList
+               });
+
+               if (response.status == 200) {
+                const parsedResponse:UserDataType = JSON.parse(await response.text())
+                return parsedResponse
+               }
+               const parsedResponse:UserDataType =JSON.parse(datasample)
+               return parsedResponse
         }
 
 }
+
+const datasample= `
+{
+    "id": 2,
+    "user": "Administrador",
+    "games": 122,
+    "sub": false,
+    "credits": 15,
+    "days": null
+  }
+`
